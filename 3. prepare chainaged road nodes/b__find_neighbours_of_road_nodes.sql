10099f3f35c60451e7858e43716ab0570adf79d7
Christoph Jung <cj.christoph.jung@me.com>
2018-02-20 06:18:59 +0100

    find neighbours of road nodes

--- 3. prepare chainaged road nodes/b__find_neighbours_of_road_nodes.sql
+++ 3. prepare chainaged road nodes/b__find_neighbours_of_road_nodes.sql
@@ -1,7 +1,7 @@
 DO $$
 	DECLARE
 		equidistance CHARACTER VARYING := '100';
-        line_id CHARACTER VARYING := 'id';
+        line_id CHARACTER VARYING := 'edge_id';
         point_id CHARACTER VARYING := 'id';
 		predecessor INTEGER := NULL;
         i record;
@@ -9,35 +9,38 @@ DO $$
         k record;
 	BEGIN
 		--iterate through the IDs of all lines of the road network
-        [ <<first_loop>> ]
+        << first_loop >>
+        --using execute command
 		FOR i IN SELECT line_id FROM roads.roads_rdbl LOOP
 			
 			--iterate through all nodes on the current line in ASCENDING order of its number on line
-			[ <<second_loop>> ]
+			<< second_loop >>
             FOR j IN SELECT * FROM roads.road_nodes_rdbl WHERE old_id = i.line_id ORDER BY number_on_line ASC LOOP
 				
 				--if no predecessor is set, than it is the first node on the current line
-				IF equidistance != NULL
-				THEN UPDATE roads.road_nodes_rdbl SET neighbours = neighbours || '{{predecessor::CHARACTER VARYING, equidistance}}' WHERE point_id = j.point_id;
+				IF equidistance IS NOT NULL THEN
+                	UPDATE roads.road_nodes_rdbl SET neighbours = neighbours || '{{predecessor::CHARACTER VARYING, equidistance}}' WHERE point_id = j.point_id;
+                END IF;
 				
 				--save the ID of the current dataset as predecessor of the next loop
 				predecessor := j.point_id;
 				
-			END LOOP [ second_loop ];
+			END LOOP second_loop;
 			
 			--iterate through all nodes on the current line in DESCENDING order of its number on line
 			predecessor := NULL;
-            [ <<third_loop>> ]
+            << third_loop >>
 			FOR k IN SELECT * FROM roads.road_nodes_rdbl WHERE old_id = i.line_id ORDER BY number_on_line DESC LOOP
 				
 				--if no predecessor is set, than it is the first node on the current line
-				IF equidistance != NULL
-				THEN UPDATE roads.road_nodes_rdbl SET neighbours = neighbours || '{{predecessor::CHARACTER VARYING, equidistance}}' WHERE point_id = j.point_id;
+				IF equidistance IS NOT NULL THEN
+                	UPDATE roads.road_nodes_rdbl SET neighbours = neighbours || '{{predecessor::CHARACTER VARYING, equidistance}}' WHERE point_id = j.point_id;
+                END IF;
 				
 				--save the ID of the current dataset as predecessor of the next loop
 				predecessor := j.point_id;
 				
-			END LOOP [ third_loop ];
+			END LOOP third_loop;
 			
-		END LOOP [ first_loop ];
-END $$
+		END LOOP first_loop;
+END $$ LANGUAGE 'plpgsql';
