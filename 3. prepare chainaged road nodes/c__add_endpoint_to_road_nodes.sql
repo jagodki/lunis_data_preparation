fc18becb733b0b860f52d40bdff256614ad7067b
Christoph Jung <cj.christoph.jung@me.com>
2018-02-23 17:54:13 +0100

    add endpoints of roads to the changed layer

--- 3. prepare chainaged road nodes/c__add_endpoint_to_road_nodes.sql
+++ 3. prepare chainaged road nodes/c__add_endpoint_to_road_nodes.sql
@@ -1,3 +1,12 @@
-WITH roads_temp AS (SELECT edge_id, ST_NPoints(geom) AS count_vertices, ST_Endpoint(geom) AS endpoint FROM roads.roads_rdbl)
-INSERT INTO roads.road_nodes_rdbl(old_id, geom, number_on_line)
-SELECT edge_id, endpoint, (count_vertices + 1) FROM roads_temp;
+WITH roads_temp AS (
+		SELECT edge_id, ST_Endpoint(geom) AS endpoint FROM roads.roads_rdbl
+	),	nodes_temp AS (
+		SELECT old_id, MAX(number_on_line) AS max_number_on_line FROM chainage.roads_rdbl_chainage GROUP BY old_id
+	)
+INSERT INTO chainage.roads_rdbl_chainage(
+	old_id,
+	geom,
+	number_on_line
+) SELECT r.edge_id, r.endpoint, (n.max_number_on_line + 1)
+	FROM roads_temp r, nodes_temp n
+	WHERE r.edge_id = n.old_id;
