ee480ab27bb4f8f4bac5073b7e341ba3b8a5c6ec
Christoph Jung <cj.christoph.jung@me.com>
2018-03-14 06:22:38 +0100

    reorganize code

--- 3. edit chainaged road nodes/a__prepare_road_data.sql
+++
@@ -1,5 +0,0 @@
---add additional columns to the table of the chainaged road network
-ALTER TABLE roads.road_nodes_rdbl
-ADD COLUMN neighbours CHARACTER VARYING[][],
-ADD COLUMN nearest_neighbour INTEGER,
-ADD COLUMN cumulated_distance DOUBLE PRECISION;
