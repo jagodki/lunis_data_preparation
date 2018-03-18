1f1fafbc0118df032ebd73640dedd0d12893951d
Christoph Jung <cj.christoph.jung@me.com>
2018-03-18 15:26:08 +0100

    update calculation of catchment areas

---
+++ 5. create table structur for POI/b__add_columns_to_store_result.sql
@@ -0,0 +1,11 @@
+﻿--add columns dynamically to the road nodes for storing the distances to each POI
+DO $BODY$
+DECLARE
+	i RECORD;
+BEGIN
+       FOR i IN SELECT id FROM lunis.schools LOOP
+               EXECUTE 'ALTER TABLE pgchainage.roads_rdbl_chainage ADD COLUMN school_id_' || i.id || ' double precision;';
+       END LOOP;
+END
+$BODY$
+
--- 6. create catchement areas/a__routing.sql
+++ 6. create catchement areas/a__routing.sql
@@ -17,3 +17,22 @@ select
        id,
 	   false)) as foo ) as cost
 from pgchainage.roads_rdbl_chainage;
+
+--create catchment area from all road nodes to each POI
+DO $BODY$
+DECLARE
+	i RECORD;
+BEGIN
+       FOR i IN SELECT id, geom FROM lunis.schools LOOP
+               EXECUTE
+                       'UPDATE pgchainage.pgchainage.roads_rdbl_chainage c' ||
+                       'SET school_id_' || i.id || '= foo2.cost' ||
+                       'FROM (' ||
+                       'SELECT id, (SELECT SUM(cost) FROM (SELECT * FROM pgr_dijkstra(''' ||
+                       'SELECT id, start_id AS source, end_id AS target, cost FROM pgchainage.roads_rdbl_substring'',' ||
+                       'geom, id, FALSE)) AS foo) AS cost' ||
+                       'from pgchainage.roads_rdbl_chainage) foo2' ||
+                       'WHERE c.id = foo.id;';
+       END LOOP;
+END
+$BODY$
