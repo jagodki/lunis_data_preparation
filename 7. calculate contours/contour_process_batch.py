1b17ddb539531957a4630ee10b9569b2e5575ca5
Christoph Jung <cj.christoph.jung@me.com>
2018-05-25 16:17:29 +0200

    update script to create contour lines

--- 7. calculate contours/contour_process_batch.py
+++ 7. calculate contours/contour_process_batch.py
@@ -4,25 +4,26 @@ import time, sys
 
 #define the name of the input layer and the absolute path
 #of the output directory
-input_layer = ""
-output_directory = ""
+input_layer = "edge_data_chainage"
+output_directory = "/Users/Christoph/Desktop"
+equidistance = 500
 
 def get_searched_layer(layer_list, searched_layer):
     '''This function searches through all loaded layers using their names.'''
-    for layer in layer_list:
-        if layer.name() == searched_layer:
+    for name, layer in layer_list.items():
+        if name.startswith(searched_layer):
             return layer
 
 def get_count_of_school_fields(layer):
     '''This function iterates over all fields of a given layer and returns the count of fields starting with "school".'''
     count = 0
     for field in layer.fields():
-        if field.name() == "school*":
+        if field.name().startswith("school"):
             count += 1
     return count
 
 def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
-    """
+    '''
         Call in a loop to create terminal progress bar
         @params:
         iteration   - Required  : current iteration (Int)
@@ -32,22 +33,30 @@ def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, l
         decimals    - Optional  : positive number of decimals in percent complete (Int)
         length      - Optional  : character length of bar (Int)
         fill        - Optional  : bar fill character (Str)
-        """
+        '''
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
+    filledLength = int(length * iteration / total)
     bar = fill * filledLength + '-' * (length - filledLength)
     print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
     # Print New Line on Complete
     if iteration == total:
         print()
 
-def add_null_filter(layer):
-    return 1
+def add_null_filter(layer, field_name):
+    layer.setSubsetString(field_name + " is not null")
 
-def remove_null_filter():
-    return 1
+def remove_null_filter(layer):
+    layer.setSubsetString("")
 
-def create_parameters():
+def create_parameters(layer, field_name):
+    #get the highest value in the given field
+    field_id = layer.dataProvider().fieldNameIndex(field_name)
+    maximum_value = layer.maximumValue(field_id)
+    
+    #calculate the count of needed contours depending of the equidistance
+    count_of_contours = (int) (maximum_value / equidistance)
+    if (maximum_value % equidistance) != 0:
+        count_of_contours = count_of_contours + 1
     return 1
 
 #get the searched vector layer
@@ -60,16 +69,21 @@ total_iterations = get_count_of_school_fields(vector_layer)
 i = 0
 printProgressBar(i, total_iterations, prefix = 'Progress:', suffix = 'Complete', length = 50)
 for field in vector_layer.fields():
-    if field.name() == "school*":
+    if field.name().startswith("school"):
         
         #add a filter expression to the current field
-        add_null_filter(vector_layer)
-
+        add_null_filter(vector_layer, field.name())
+        
         #start the processing
-        parameters = create_parameters()
-        processing.run('Contour:contour', parameters)
-
+        parameters = create_parameters(vector_layer, field.name())
+        try:
+            processing.run('Contour:contour', parameters)
+        except:
+            print("Cannot process field " + field.name())
+        
+        #remove null filter
+        remove_null_filter(vector_layer)
+        
         #update progressbar
         printProgressBar(i + 1, total_iterations, prefix = 'Progress:', suffix = 'Complete', length = 50)
         i = i + 1
-
