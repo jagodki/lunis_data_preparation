49a1bef832dd3edbf9db24050013880f82c2eed8
Christoph Jung <jagodki.cj@gmail.com>
2019-08-02 10:03:52 +0200

    remove command from a necessary query

--- 2. prepare road network for chainage/a__clean_up_road_network.sql
+++ 2. prepare road network for chainage/a__clean_up_road_network.sql
@@ -6,4 +6,4 @@ SELECT topology.CreateTopology('roads_topo', 25833, 0.5);
 SELECT topology.AddTopoGeometryColumn('roads_topo', 'roads', 'roads_single_geom', 'topo_geom', 'LINESTRING');
 
 --update the topo-geometry
---UPDATE roads.roads_single_geom SET topo_geom = topology.toTopoGeom(ST_Transform(geom, 25833), 'roads_topo', 1, 0.5);
+UPDATE roads.roads_single_geom SET topo_geom = topology.toTopoGeom(ST_Transform(geom, 25833), 'roads_topo', 1, 0.5);
